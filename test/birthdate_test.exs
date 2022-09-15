277480d64331829abea9d8dc8d43c6d26807e18d
Larry Baltz <larry@baltz.org>
2022-09-15 09:59:52 +0200

    some seudocode tests #WIP

--- test/birthdate_test.exs
+++ test/birthdate_test.exs
@@ -2,7 +2,20 @@ defmodule BirthdateTest do
   use ExUnit.Case
   doctest Birthdate
 
-  test ".new" do
-    assert Birthdate.new() == :world
+  defp today(), do: ~D[2022-06-15]
+  defp future(), do: Date.add(today, 2)
+
+  describe ".new" do
+    test "accepts recent date" do
+      assert {:ok, _} == Birthdate.new(~D[1965-03-24])
+    end
+
+    test "accepts recent date with explicit today" do
+      assert {:ok, _} == Birthdate.new(~D[1965-03-24], today())
+    end
+
+    test "rejects future date" do
+      assert {:error, "future date"} == Birthdate.new(future(), today())
+    end
   end
 end
